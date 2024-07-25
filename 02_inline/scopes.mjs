b137cd46ba49d60f20a9a4f087baa84b765be452
Simon Zünd <szuend@chromium.org>
2024-07-25 09:25:13 +0200

    Add global variables to 02_inline

--- 02_inline/scopes.mjs
+++ 02_inline/scopes.mjs
@@ -3,7 +3,7 @@ import { GeneratedRangeBuilder, OriginalScopeBuilder } from "../scopes_builder.m
 
 export function scopes(names) {
   const originalScopes = [new OriginalScopeBuilder(names)
-    .start(0, 0, 'global')
+    .start(0, 0, 'global', undefined, ['globalThis', 'log', 'inner', 'outer'])
     .start(2, 17, 'function', 'log', ['x'])
     .end(4, 1)
     .start(7, 19, 'function', 'inner', ['y'])
@@ -14,7 +14,7 @@ export function scopes(names) {
     .build()];
   
   const generatedRanges = new GeneratedRangeBuilder(names)
-    .start(0, 0, { definition: { sourceIdx: 0, scopeIdx: 0 }})
+    .start(0, 0, { definition: { sourceIdx: 0, scopeIdx: 0 }, bindings: ['globalThis']})
     .start(0, 0, { definition: { sourceIdx: 0, scopeIdx: 5 }, callsite: { sourceIdx: 0, line: 16, column: 0 }, bindings: ['42']})
     .start(0, 0, { definition: { sourceIdx: 0, scopeIdx: 3 }, callsite: { sourceIdx: 0, line: 13, column: 2 }, bindings: ['42']})
     .start(0, 0, { definition: { sourceIdx: 0, scopeIdx: 1 }, callsite: { sourceIdx: 0, line: 8, column: 2}, bindings: ['42']})
